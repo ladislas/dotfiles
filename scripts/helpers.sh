82f20295ac5c835a2c4a828367296ed721154167
Ladislas de Toldi <ladislas@detoldi.me>
2020-01-16 22:41:18 +0100

    output time

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -41,7 +41,7 @@ function try {
 
 	# output result
 	if [ $cmd_result -eq 0 ]; then
-		echo "✅ ($runtime)"
+		echo "✅ ($end - $start = $runtime)"
 		if [ $super_verbose = true ]; then
 			cat $tmp_file
 		fi
